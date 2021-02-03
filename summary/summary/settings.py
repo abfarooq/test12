1b5da439468eba5c0735dfe00c0b4609933e0acc
abfarooq <abdullah.farooq@siparadigm.com>
2021-02-03 23:20:07 +0500

    no message

--- summary/summary/settings.py
+++ summary/summary/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'summary.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'NAME': 'hris',
+        'USER': 'abfarooq',
+        'PASSWORD': 'password'
+
     }
+
 }
 
 
